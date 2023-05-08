131a547ad5de5362ee51967c40dd1ada31112d03
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-08 03:20:18 -0400

    Testando a entrada de uma linha inteira de dados genéricos.

--- estudo_java/src/com/loiane/javacourse/c12/LeituraDadosTeclado.java
+++ estudo_java/src/com/loiane/javacourse/c12/LeituraDadosTeclado.java
@@ -7,8 +7,17 @@ public class LeituraDadosTeclado {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
+		System.out.println("Digite seu nome");
 		
-		System.out.println("testando");
+		String nomeCompleto = scan.nextLine();
+		System.out.print("Seu nome completo é: " + nomeCompleto);
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
