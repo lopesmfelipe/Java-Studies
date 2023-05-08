64560b18d51f956ff39cbe885234692594d01106
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-08 03:13:23 -0400

    Nova classe sobre entrada de dados.

---
+++ estudo_java/src/com/loiane/javacourse/c12/LeituraDadosTeclado.java
@@ -0,0 +1,14 @@
+package com.loiane.javacourse.c12;
+
+import java.util.Scanner;
+
+public class LeituraDadosTeclado {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("testando");
+	}
+
+}
