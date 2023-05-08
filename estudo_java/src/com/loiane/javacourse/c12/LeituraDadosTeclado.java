ba514d9f21426b87cc363635257db5358c594981
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-08 03:28:21 -0400

    Testando diferentes métodos da Classe Scanner

--- estudo_java/src/com/loiane/javacourse/c12/LeituraDadosTeclado.java
+++ estudo_java/src/com/loiane/javacourse/c12/LeituraDadosTeclado.java
@@ -7,10 +7,14 @@ public class LeituraDadosTeclado {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Digite seu nome");
+		System.out.println("Digite seu nome completo");
 		
-		String nomeCompleto = scan.nextLine();
-		System.out.print("Seu nome completo é: " + nomeCompleto);
+		String nomeCompleto = scan.nextLine();  // Vai receber todo o valor digitado
+		System.out.println("Seu nome completo é: " + nomeCompleto);
+		
+		System.out.println("Digite seu primeiro nome");
+		String primeiroNome = scan.next();  // Só vai receber o primeiro valor digitado
+		System.out.println("Seu primeiro nome é: " + primeiroNome);
 		
 		
 		
