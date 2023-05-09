3793b0b3b9cc2a28b73b69e76a882ad4cce3440c
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-09 04:09:01 -0400

    Nova classe para o estudo de Operadores aritméticos.

---
+++ estudo_java/src/com/loiane/javacourse/c13/OperadoresAritmeticos.java
@@ -0,0 +1,31 @@
+package com.loiane.javacourse.c13;
+
+public class OperadoresAritmeticos {
+
+	public static void main(String[] args) {
+		
+		int resultado = 1 + 90;
+		
+		
+		System.out.println(resultado);
+		
+		resultado = resultado - 2;
+		System.out.println(resultado);
+		
+		resultado = resultado / 4;
+		System.out.println(resultado);
+		
+		resultado = resultado * 3;
+		System.out.println(resultado);
+		
+		resultado = resultado % 3;
+		System.out.println(resultado);
+		
+		resultado++;
+		System.out.println(resultado);
+		
+		
+		
+	}
+
+}
