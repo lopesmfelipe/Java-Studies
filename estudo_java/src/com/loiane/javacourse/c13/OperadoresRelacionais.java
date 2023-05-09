a8c3bdf74ccbc6e1fb9041a10e74a04158c41e9c
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-09 04:20:10 -0400

    Adicionado elementos.

--- estudo_java/src/com/loiane/javacourse/c13/OperadoresRelacionais.java
+++ estudo_java/src/com/loiane/javacourse/c13/OperadoresRelacionais.java
@@ -4,8 +4,14 @@ public class OperadoresRelacionais {
 
 	public static void main(String[] args) {
 
+		int v1 = 2;
+		int v2 = 100;
 		
-		
+		System.out.println("v1 == v2: " + (v1 == v2));
+		System.out.println("v1 != v2: " + (v1 != v2));
+		System.out.println("v1 >= v2: " + (v1 >= v2));
+		System.out.println("v1 <= v2: " + (v1 <= v2));
+
 	}
 
 }
