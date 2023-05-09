bf9ceafcfaf1a58c286257b8dc21ca3fad99fd17
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-09 04:45:39 -0400

    Testando operadores relacionais.

--- estudo_java/src/com/loiane/javacourse/c13/OperadoresLogicos.java
+++ estudo_java/src/com/loiane/javacourse/c13/OperadoresLogicos.java
@@ -4,7 +4,12 @@ public class OperadoresLogicos {
 
 	public static void main(String[] args) {
 		
+		int valor01 = 0;
+		int valor02 = 1;
 		
+		boolean resultado1 = (valor01 == 10) && (valor02 == 1);
+		
+		System.out.println("valor01 é 10 AND valor02 é 1: " + resultado1);
 		
 		
 		
