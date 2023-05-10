35d65a6cf837cb95a90f20cd1a972e7559a408f3
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-10 05:44:45 -0400

    Novo pacote de classes sobre "if" e "else"

---
+++ estudo_java/src/com/loiane/javacourse/c14/IfElse.java
@@ -0,0 +1,35 @@
+package com.loiane.javacourse.c14;
+
+import java.util.Scanner;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Insira sua idade abaixo");
+		
+		int idade = scanner.nextInt();
+		
+		if (idade >= 18) {
+			System.out.println("Maior de idade");
+		} else {
+			System.out.println("Menor de idade");
+		}
+		
+		System.out.println("Insira o preço do produto");
+		double preco = scanner.nextDouble();
+		
+		if (preco >= 49) {
+			System.out.println("Preço ultrapassou o orçamento, não comprar");
+		} else if (30 < preco) {
+			System.out.println("Preço dentro do orçamento, mas pergunte por algum desconto");
+		} else {
+			System.out.println("Preço dentro do orçamento, comprar");
+		}
+		
+		
+	}
+
+}
