4572a277eef511ce14aaf8424b1eb70fc3d440a6
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-10 01:42:37 -0400

    Adição de impressões na classe OperadoresLogicos.

--- estudo_java/src/com/loiane/javacourse/c13/OperadoresLogicos.java
+++ estudo_java/src/com/loiane/javacourse/c13/OperadoresLogicos.java
@@ -13,8 +13,14 @@ public class OperadoresLogicos {
 		boolean resultado2 = (valor0 == 10) || (valor1 == 1);
 		System.out.println("valor0 é 10 OR valor1 é 1: " + resultado2);
 
+		boolean verdadeiro = true;
+		boolean falso = false;
 		
-		
+		System.out.println(verdadeiro && falso);
+		System.out.println(falso || verdadeiro);
+		System.out.println(!verdadeiro ^ falso);
+		System.out.println(! verdadeiro && verdadeiro); // Não verdadeiro "!"
+ 
 		
 		
 		
