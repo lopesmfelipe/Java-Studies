1c3eebdd08cc5f11e5195c1812bac073f424f330
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-10 01:27:55 -0400

    Adição de uma nova classe "Curto Circuito".

---
+++ estudo_java/src/com/loiane/javacourse/c13/CurtoCircuito.java
@@ -0,0 +1,23 @@
+package com.loiane.javacourse.c13;
+
+public class CurtoCircuito {
+
+	public static void main(String[] args) {
+		
+		boolean verdadeiro = true;
+		boolean falso = false;
+		
+		boolean resultado1 = falso && verdadeiro;
+		boolean resultado2 = falso & verdadeiro;
+		boolean resultado3 = verdadeiro && falso;
+		boolean resultado4 = verdadeiro & falso;
+		
+		System.out.println(resultado1 == verdadeiro);
+		System.out.println(resultado1);
+		System.out.println(resultado2);
+		System.out.println(resultado3);
+		System.out.println(resultado4 == verdadeiro);
+		
+	}
+
+}
