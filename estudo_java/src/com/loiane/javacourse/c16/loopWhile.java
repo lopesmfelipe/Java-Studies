fd4f107e27bc670f6c99e671091ecc30f2ea7be4
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-10 16:11:46 -0400

    Nova classe c16.

---
+++ estudo_java/src/com/loiane/javacourse/c16/loopWhile.java
@@ -0,0 +1,17 @@
+package com.loiane.javacourse.c16;
+
+public class loopWhile {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		int valorMaximo = 100;
+		
+		while (i <= valorMaximo) {
+			System.out.println("Valor da variável i: " + i);
+			i++;
+		}
+		System.out.println(i);
+	}
+
+}
