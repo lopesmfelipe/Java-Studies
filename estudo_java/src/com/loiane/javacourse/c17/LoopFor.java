9971f9d1177439dbc074e12ecedd91688058080f
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-11 03:41:29 -0400

    Modificação para organizar melhor o código.

--- estudo_java/src/com/loiane/javacourse/c17/LoopFor.java
+++ estudo_java/src/com/loiane/javacourse/c17/LoopFor.java
@@ -4,13 +4,33 @@ public class LoopFor {
 
 	public static void main(String[] args) {
 		
-		for (int i = 0; i <= 10; i++ ) {
-			System.out.println(i);
+
+		// for com mais de uma inicialização e mais de uma atualização, separados por vírgula dentro de suas respectivas áreas:
+		for (int i = 0, j = 50, a = 90; i <= a; i += 10, j -= 10) { 
+			System.out.println("valor de i: " + i + " || " + "valor de j: " + j);
+		}
+	
+		
+		
+		// "for" com partes ausentes
+		int count = 0;
+		for ( ; count < 10 ; ) {      
+			System.out.println("Valor de count: " + count);
+			count += 2;
 		}
 		
 		
 		
+		// for sem corpo, sem chaves:
+		int soma = 0;
+		for (int i = 1; i < 5; soma += i += 3); 
+		System.out.println("O valor da soma é: " + soma);
 		
+		
+
+		/*loop infinito:*/
+		
+		/*for ( ; ; );*/ 
 		
 		
 		
