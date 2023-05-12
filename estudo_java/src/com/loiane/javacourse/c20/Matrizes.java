04f29fc5e1a03720a8e2e455346b4466b80f0257
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-12 17:19:04 -0400

    Nova classe para teste de matrizes.

---
+++ estudo_java/src/com/loiane/javacourse/c20/Matrizes.java
@@ -0,0 +1,26 @@
+package com.loiane.javacourse.c20;
+
+public class Matrizes {
+
+	public static void main(String[] args) {
+		
+		double[][] competidores = new double[3][4];  
+		
+		competidores[0][0] = 10;
+		competidores[0][1] = 8;
+		competidores[0][2] = 9;
+		competidores[0][3] = 9;
+		
+		competidores[1][0] = 5;
+		competidores[1][1] = 10;
+		competidores[1][2] = 6;
+		competidores[1][3] = 4;
+		
+		competidores[2][0] = 7;
+		competidores[2][1] = 8;
+		competidores[2][2] = 9;
+		competidores[2][3] = 10;
+		
+	}
+
+}
