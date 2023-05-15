766fd1830d8194b7ff2a8cf48b0488690c8a4692
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-15 02:27:57 -0400

    comments

--- estudo_java/src/com/loiane/javacourse/c21/ForEach.java
+++ estudo_java/src/com/loiane/javacourse/c21/ForEach.java
@@ -20,15 +20,21 @@ public class ForEach {
 		}
 		
 		
+		/*Se a variável que recebe valores for de um tipo primitivo, a leitura de teclado não funciona, ou seja, não da para settar a 
+		  variável recebendo algum dado do teclado.
+		  
+		  Porém se for um objeto é possível settar recebendo dados do teclado*/
+		
+		
 		System.out.println("Usando o for each");
 		
-		
-		for (int nota : notas) { // Boas práticas no "for each" o nome da variável que vai receber os valores do array ou lista, ser o nome singular desse array ou lista 
+		for (int nota : notas) { 
 			System.out.println(nota);
 		}
 		
+	 /*Boas práticas no "for each" o nome da variável que vai receber os valores do array ou lista, ser o nome singular desse array
+	   ou lista*/
 		
-		
-	}
+	}	
 
 }
