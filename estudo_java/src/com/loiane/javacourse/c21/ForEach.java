16f80e85f6f390f719a8f8ed9bdaee12d4b2136c
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-15 02:01:24 -0400

    Teste da classe Ramdom.

--- estudo_java/src/com/loiane/javacourse/c21/ForEach.java
+++ estudo_java/src/com/loiane/javacourse/c21/ForEach.java
@@ -1,11 +1,24 @@
 package com.loiane.javacourse.c21;
 
+import java.util.Random;
+
 public class ForEach {
 
 	public static void main(String[] args) {
 		
+		int[] notas = new int[10];
 		
+		Random random = new Random();
 		
+		for (int i = 0; i < notas.length; i++) {
+			
+			notas[i] = random.nextInt(10);
+			
+		}
+		
+		for (int i = 0; i < notas.length; i++) {
+			System.out.println(notas[i]);
+		}
 	}
 
 }
