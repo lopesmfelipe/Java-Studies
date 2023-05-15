02b168e428ae3a9f9ccec8a1396f7dab4ad6cf78
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-15 03:12:21 -0400

    percorrendo matriz com for each.

--- estudo_java/src/com/loiane/javacourse/c21/ForEach.java
+++ estudo_java/src/com/loiane/javacourse/c21/ForEach.java
@@ -20,8 +20,7 @@ public class ForEach {
 		}
 		
 		
-		/*Se a variável que recebe valores for de um tipo primitivo, a leitura de teclado não funciona, ou seja, não da para settar a 
-		  variável recebendo algum dado do teclado.
+		/*Se a variável que recebe valores for de um tipo primitivo, não da para settar a variável recebendo algum dado do teclado.
 		  
 		  Porém se for um objeto é possível settar recebendo dados do teclado*/
 		
@@ -34,6 +33,38 @@ public class ForEach {
 		
 	 /*Boas práticas no "for each" o nome da variável que vai receber os valores do array ou lista, ser o nome singular desse array
 	   ou lista*/
+		
+		System.out.println();
+		
+		// exemplo 02
+		
+			double[][] notasPessoas = new double[3][4];  
+		
+			notasPessoas[0][0] = 7;
+			notasPessoas[0][1] = 6;
+			notasPessoas[0][2] = 8;
+			notasPessoas[0][3] = 10;
+		
+			notasPessoas[1][0] = 6;
+			notasPessoas[1][1] = 5;
+			notasPessoas[1][2] = 7;
+			notasPessoas[1][3] = 7;
+		
+			notasPessoas[2][0] = 7;
+			notasPessoas[2][1] = 8;
+			notasPessoas[2][2] = 9;
+			notasPessoas[2][3] = 10;
+		
+			
+			// for each aninhado para percorrer a matriz
+			for (double[] notasPessoa : notasPessoas) {
+				for(double nota : notasPessoa) {
+					System.out.println(nota + " ");
+				}
+				System.out.println();
+			}
+			
+		
 		
 	}	
 
