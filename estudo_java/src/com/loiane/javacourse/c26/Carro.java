2864dc77111327632d545208579f95d0d2f35691
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-16 07:02:34 -0400

    update

--- estudo_java/src/com/loiane/javacourse/c26/Carro.java
+++ estudo_java/src/com/loiane/javacourse/c26/Carro.java
@@ -2,6 +2,16 @@ package com.loiane.javacourse.c26;
 
 public class Carro {
 	
+	String marca; 
+	String modelo;
+	int numeroPassageiros;
+	double capacidadeCombustivel;
+	double consumoCombustivel;
+	double velocidadeMaxima;
+	
+	void exibirAutonomiaCombustivel() {
+		System.out.println("A autonoma do carro é: " + capacidadeCombustivel * consumoCombustivel + "km");
+	}
 	
 	
 	
