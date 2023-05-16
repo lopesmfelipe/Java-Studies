36c76a2aaa3a2ff0bb12c809387858db4d5ce0f7
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-15 23:45:01 -0400

    adicionada nova classe c24

---
+++ estudo_java/src/com/loiane/javacourse/c24/CarroEsportivo.java
@@ -0,0 +1,15 @@
+package com.loiane.javacourse.c24;
+
+public class CarroEsportivo {
+	
+	String marca; 
+	String modelo;
+	int numeroPassageiros;
+	double capacidadeCombustivel;
+	double consumoCombustivel;
+	double velocidadeMaxima;
+	
+	
+	
+	
+}
