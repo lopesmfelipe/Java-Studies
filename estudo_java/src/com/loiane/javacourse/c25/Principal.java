d5884161050bd0120da5be939e681da6a59b1dd0
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-16 07:01:29 -0400

    novo pacote

---
+++ estudo_java/src/com/loiane/javacourse/c25/Principal.java
@@ -0,0 +1,35 @@
+package com.loiane.javacourse.c25;
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Carro mustang = new Carro();
+		Carro audi = new Carro();
+		
+		
+		mustang.velocidadeMaxima = 270.8;
+		mustang.numeroPassageiros = 2;
+		mustang.capacidadeCombustivel = 45;
+		mustang.consumoCombustivel = 0.8;
+		mustang.marca = "Ford";
+		
+		audi.velocidadeMaxima = 290.4;
+		audi.numeroPassageiros = 4;
+		audi.capacidadeCombustivel = 41;
+		audi.consumoCombustivel = 0.4;
+		audi.marca = "audi";
+		
+		System.out.println(mustang.marca);
+		System.out.println(mustang.velocidadeMaxima);
+		mustang.exibirAutonomiaCombustivel();
+		
+		System.out.println();
+		
+		System.out.println(audi.marca);
+		System.out.println(audi.velocidadeMaxima);
+		audi.exibirAutonomiaCombustivel();
+	}
+
+}
---
+++ estudo_java/src/com/loiane/javacourse/c26/Principal.java
@@ -0,0 +1,12 @@
+package com.loiane.javacourse.c26;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
+
+}
