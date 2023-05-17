999c84a3ae7fb0b8b1c4a58c204b783026709f03
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-16 23:53:38 -0400

    update

--- estudo_java/src/com/loiane/javacourse/c27/Carro.java
+++ estudo_java/src/com/loiane/javacourse/c27/Carro.java
@@ -2,6 +2,22 @@ package com.loiane.javacourse.c27;
 
 public class Carro {
 
+	String marca; 
+	String modelo;
+	int numeroPassageiros;
+	double capacidadeCombustivel;
+	double consumoCombustivel;
+	double velocidadeMaxima;
 	
+	void exibirAutonomiaCombustivel() {
+		System.out.println("A autonoma do carro é: " + capacidadeCombustivel * consumoCombustivel + "km");
+	}
+	
+	double obterAutonomia() {
+		
+		System.out.println("Método obterAutonomia foi chamado");
+		
+		return capacidadeCombustivel * consumoCombustivel;
+	}
 	
 }
