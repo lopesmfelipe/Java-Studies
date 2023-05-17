36083e395b6555d3e6ff9f917f8000d3a769b80f
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-17 03:33:21 -0400

    nova classe c28

---
+++ estudo_java/src/com/loiane/estudo_java/c28/Main.java
@@ -0,0 +1,21 @@
+package com.loiane.estudo_java.c28;
+
+import com.loiane.javacourse.c11.*;  // dessa forma você importa todas as classe de um pacote de uma vez, sem precisar especificar todas
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
