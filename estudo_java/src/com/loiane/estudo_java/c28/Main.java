0c0e727a1246143df1b37ab2ac2b821d44a096c0
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-17 03:35:40 -0400

    comments

--- estudo_java/src/com/loiane/estudo_java/c28/Main.java
+++ estudo_java/src/com/loiane/estudo_java/c28/Main.java
@@ -1,6 +1,6 @@
 package com.loiane.estudo_java.c28;
-
-import com.loiane.javacourse.c11.*;  // dessa forma você importa todas as classe de um pacote de uma vez, sem precisar especificar todas
+									 // prefira especificar e deixar claro tudo que esta importando
+import com.loiane.javacourse.c11.*;  // dessa forma você importa todas as classe de um pacote de uma vez, sem precisar especificar todas, porém é feio
 
 public class Main {
 
