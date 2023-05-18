6d178c0700ac92ab5277156409cbd22739b2a6a1
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-17 22:40:17 -0400

    update

--- estudo_java/src/com/loiane/estudo_java/c29/TesteCarro.java
+++ estudo_java/src/com/loiane/estudo_java/c29/TesteCarro.java
@@ -14,10 +14,12 @@ public class TesteCarro {
 		
 		Carro esportivo2 = new Carro("Elétrico", "McLaren", 275, 2, 50, 1.2);
 		
+		System.out.println(esportivo2.tipo);
 		System.out.println(esportivo2.marca);
-		
-		
-		
+		System.out.println(esportivo2.velocidadeMaxima);
+		System.out.println(esportivo2.numeroPassageiros);
+		System.out.println(esportivo2.capacidadeCombustivel);
+		System.out.println(esportivo2.consumoCombustivel);
 	}
 
 }
