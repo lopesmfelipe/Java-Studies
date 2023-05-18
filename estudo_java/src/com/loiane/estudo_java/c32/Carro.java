c920205d4d5282d12b52d1af00b038134f6602a9
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-18 06:57:07 -0400

    novo pacote c32

---
+++ estudo_java/src/com/loiane/estudo_java/c32/Carro.java
@@ -0,0 +1,14 @@
+package com.loiane.estudo_java.c32;
+
+public class Carro {
+	
+	String tipo;
+	String marca;
+	double velocidadeMaxima;
+	int numeroPassageiros;
+	double capacidadeCombustivel;
+	double consumoCombustivel;
+	
+	
+	
+}
---
+++ estudo_java/src/com/loiane/estudo_java/c32/Teste.java
@@ -0,0 +1,13 @@
+package com.loiane.estudo_java.c32;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+	}
+
+}
