9a21f451e523c69060747551cfddae423059c798
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-18 06:10:25 -0400

    update

---
+++ estudo_java/src/com/loiane/estudo_java/c31/TesteCarro.java
@@ -0,0 +1,16 @@
+package com.loiane.estudo_java.c31;
+
+import com.loiane.estudo_java.c30.Carro;
+
+public class TesteCarro {
+
+	public static void main(String[] args) {
+
+		
+		Carro Honda = new Carro();
+		
+		
+		
+	}
+
+}
