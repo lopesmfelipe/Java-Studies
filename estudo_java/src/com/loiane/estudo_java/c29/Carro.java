395830f4bc024f264041ef5fcd8c47b70ffb65c6
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-18 00:31:28 -0400

    format code

--- estudo_java/src/com/loiane/estudo_java/c29/Carro.java
+++ estudo_java/src/com/loiane/estudo_java/c29/Carro.java
@@ -1,49 +1,47 @@
 package com.loiane.estudo_java.c29;
 
 public class Carro {
-	
+
 	String tipo;
-	String marca; 
+	String marca;
 	double velocidadeMaxima;
 	int numeroPassageiros;
 	double capacidadeCombustivel;
 	double consumoCombustivel;
-	
-	
+
 	Carro() {
 		System.out.println("Classe 'carro' foi instanciada.");
 		tipo = "elétrico";
 	}
-	
-	Carro(String tipo2, String marca2, double velocidadeMaxima2, int numeroPassageiros2, double capacidadeCombustivel2, double consumoCombustivel2) {
-		
+
+	Carro(String tipo2, String marca2, double velocidadeMaxima2, int numeroPassageiros2, double capacidadeCombustivel2,
+			double consumoCombustivel2) {
+
 		tipo = tipo2;
 		marca = marca2;
 		velocidadeMaxima = velocidadeMaxima2;
 		numeroPassageiros = numeroPassageiros2;
 		capacidadeCombustivel = capacidadeCombustivel2;
 		consumoCombustivel = consumoCombustivel2;
-		
-		
+
 	}
-	
-	
+
 	void exibirAutonomiaCombustivel() {
 		System.out.println("A autonoma do carro é: " + capacidadeCombustivel * consumoCombustivel + "km");
 	}
-	
+
 	double obterAutonomia() {
-		
+
 		System.out.println("Método obterAutonomia foi chamado");
-		
+
 		return capacidadeCombustivel * consumoCombustivel;
 	}
-	
+
 	double calcularCombustivel(double km) {
-		
+
 		double qtdCombustivel = km * consumoCombustivel;
-		
+
 		return qtdCombustivel;
 	}
-	
+
 }
--- estudo_java/src/com/loiane/estudo_java/c31/Carro.java
+++ estudo_java/src/com/loiane/estudo_java/c31/Carro.java
@@ -3,14 +3,28 @@ package com.loiane.estudo_java.c31;
 public class Carro {
 
 	String tipo;
-	String marca; 
+	String marca;
 	double velocidadeMaxima;
 	int numeroPassageiros;
 	double capacidadeCombustivel;
 	double consumoCombustivel;
-	
-	
-	
-	
-	
+
+	void exibirAutonomiaCombustivel() {
+		System.out.println("A autonoma do carro é: " + capacidadeCombustivel * consumoCombustivel + "km");
+	}
+
+	double obterAutonomia() {
+
+		System.out.println("Método obterAutonomia foi chamado");
+
+		return capacidadeCombustivel * consumoCombustivel;
+	}
+
+	double calcularCombustivel(double km) {
+
+		double qtdCombustivel = km * consumoCombustivel;
+
+		return qtdCombustivel;
+	}
+
 }
