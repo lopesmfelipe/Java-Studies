4c63b7b24ccb63fde320c62180037591b6b05fd0
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-17 23:03:36 -0400

    adição de atributos

--- estudo_java/src/com/loiane/estudo_java/c30/Carro.java
+++ estudo_java/src/com/loiane/estudo_java/c30/Carro.java
@@ -3,5 +3,12 @@ package com.loiane.estudo_java.c30;
 public class Carro {
 
 	
+	String tipo;
+	String marca; 
+	double velocidadeMaxima;
+	int numeroPassageiros;
+	double capacidadeCombustivel;
+	double consumoCombustivel;
+	
 	
 }
