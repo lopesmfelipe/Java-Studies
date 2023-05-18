73ae227df57120ff83363cecc8cbcf52e9d281de
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-17 22:52:28 -0400

    update

---
+++ estudo_java/src/com/loiane/estudo_java/c29/Carro2.java
@@ -0,0 +1,35 @@
+package com.loiane.estudo_java.c29;
+
+public class Carro2 {
+	
+	String tipo;
+	String marca; 
+	double velocidadeMaxima;
+	int numeroPassageiros;
+	double capacidadeCombustivel;
+	double consumoCombustivel;
+	
+	public Carro2() {  // Boa prática ao criar um construtor com parâmetros, sempre criar um vazio sem nada também
+		
+	}
+	
+	
+	public Carro2(String tipo, String marca, double velocidadeMaxima, int numeroPassageiros,
+			double capacidadeCombustivel, double consumoCombustivel) {
+		
+		super();
+		this.tipo = tipo;
+		this.marca = marca;
+		this.velocidadeMaxima = velocidadeMaxima;
+		this.numeroPassageiros = numeroPassageiros;
+		this.capacidadeCombustivel = capacidadeCombustivel;
+		this.consumoCombustivel = consumoCombustivel;
+		
+	}
+	
+	
+	
+	
+	
+	
+}
