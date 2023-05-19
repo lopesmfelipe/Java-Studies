29520035454cf630c263edd7c6475478fb49daa9
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-19 06:22:32 -0400

    comments

--- estudo_java/src/com/loiane/estudo_java/c34/CalculatorTest.java
+++ estudo_java/src/com/loiane/estudo_java/c34/CalculatorTest.java
@@ -11,6 +11,11 @@ public class CalculatorTest {
 																	// utiliza-los sem precisar instanciar um objeto
 		divisao2v(2, 2);
 
+		int tres = 3;
+		int quatro = 4;
+		
+		int terceiro = (int) Math.pow(tres, quatro);   // exemplo da utilização de um método da classe 'Math' sem precisar instanciar um objeto dessa classe porque é 'static'
+		System.out.println(terceiro);
 	}
 
 	static int divisao2v(int n1, int n2) { // método main consegue acessar porque o método é 'static'
