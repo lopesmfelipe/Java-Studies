6edf4af71c752ed32b64e626af353ff29075175e
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-19 05:59:38 -0400

    changes

--- estudo_java/src/com/loiane/estudo_java/c34/CalculatorTest.java
+++ estudo_java/src/com/loiane/estudo_java/c34/CalculatorTest.java
@@ -2,9 +2,12 @@ package com.loiane.estudo_java.c34;
 
 public class CalculatorTest {
 
+	static int resultadoSoma; // o método main consegue acessar por ser 'static', caso não fosse, não conseguiria
+
 	public static void main(String[] args) {
 
-		MyCalculator.divisao(new int[] { 2, 3 }); // como temos métodos static na outra classe, podemos utiliza-los sem precisar instanciar um objeto
+		resultadoSoma = MyCalculator.divisao(new int[] { 2, 3 }); // como temos métodos static na outra classe, podemos
+																	// utiliza-los sem precisar instanciar um objeto
 
 	}
 
