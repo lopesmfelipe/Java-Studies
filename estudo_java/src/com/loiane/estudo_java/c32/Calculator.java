8b947a4841e3cd402fea5883a9364c43479c97e8
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-18 20:14:23 -0400

    update

--- estudo_java/src/com/loiane/estudo_java/c32/Calculator.java
+++ estudo_java/src/com/loiane/estudo_java/c32/Calculator.java
@@ -2,11 +2,13 @@ package com.loiane.estudo_java.c32;
 
 public class Calculator {
 	
-	public int divisao(int numero1, int numero2) {
+	public double divisao(double numero1, double numero2) {
 		return numero1 / numero2;
 	}
 	
-	
+	public int divisao(int numero1, int numero2) {
+		return numero1 / numero2;
+	}
 	
 	
 	
