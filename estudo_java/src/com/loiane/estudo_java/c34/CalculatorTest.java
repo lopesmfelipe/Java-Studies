e49d6447dbabf3bd98e428f8e076cf31a0dda68d
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-19 00:24:03 -0400

    fomatted code

--- estudo_java/src/com/loiane/estudo_java/c34/CalculatorTest.java
+++ estudo_java/src/com/loiane/estudo_java/c34/CalculatorTest.java
@@ -3,12 +3,10 @@ package com.loiane.estudo_java.c34;
 public class CalculatorTest {
 
 	public static void main(String[] args) {
-		
-		
-		MyCalculator.divisao(new int[] {2,3});  // como temos métodos static na outra classe, podemos utiliza-los sem precisar instanciar um objeto
-		
-		
-		
+
+		MyCalculator.divisao(new int[] { 2, 3 }); // como temos métodos static na outra classe, podemos utiliza-los sem
+													// precisar instanciar um objeto
+
 	}
 
 }
