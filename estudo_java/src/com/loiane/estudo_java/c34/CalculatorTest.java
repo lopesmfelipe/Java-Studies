c797d5194c7ddcb285d790aaa027ac99b3c0196c
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-19 00:14:49 -0400

    update

--- estudo_java/src/com/loiane/estudo_java/c34/CalculatorTest.java
+++ estudo_java/src/com/loiane/estudo_java/c34/CalculatorTest.java
@@ -4,7 +4,10 @@ public class CalculatorTest {
 
 	public static void main(String[] args) {
 		
+		MyCalculator calcul = new MyCalculator();
 		
+		int x = calcul.divisao(4, 2);
+		System.out.println(x);
 		
 	}
 
