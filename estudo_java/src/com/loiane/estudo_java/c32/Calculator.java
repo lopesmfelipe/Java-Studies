5f20336cfee4fd586acaf556e45ca18d1dd95d09
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-18 20:18:47 -0400

    formated code

--- estudo_java/src/com/loiane/estudo_java/c32/Calculator.java
+++ estudo_java/src/com/loiane/estudo_java/c32/Calculator.java
@@ -1,32 +1,29 @@
 package com.loiane.estudo_java.c32;
 
 public class Calculator {
-	
+
 	public double divisao(double numero1, double numero2) {
 		return numero1 / numero2;
 	}
-	
+
 	public int divisao(int numero1, int numero2) {
 		return numero1 / numero2;
 	}
-	
+
 	public double divisao(double numero1, double numero2, double numero3) {
 		return numero1 / numero2;
 	}
-	
+
 	public int divisao(int[] vetor) {
-		
+
 		int total = 0;
-		
-		for (int i = 0; i < vetor.length; i ++) {
+
+		for (int i = 0; i < vetor.length; i++) {
 			total += vetor[i];
 		}
-		
+
 		return total;
-		
-		
-		
-		
+
 	}
-	
+
 }
