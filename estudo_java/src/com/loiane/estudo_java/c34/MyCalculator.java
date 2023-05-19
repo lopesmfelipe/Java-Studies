725bd629cc066f69ecd8a4c7f45f8f131f4a96d5
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-19 00:16:52 -0400

    implement static

--- estudo_java/src/com/loiane/estudo_java/c34/MyCalculator.java
+++ estudo_java/src/com/loiane/estudo_java/c34/MyCalculator.java
@@ -2,23 +2,23 @@ package com.loiane.estudo_java.c34;
 
 public class MyCalculator {
 
-	public double divisao(double numero1, double numero2) {
+	public static double divisao(double numero1, double numero2) {
 		return numero1 / numero2;
 	}
 	
-	public double divisor(double numero1, double numero2) {
+	public static double divisor(double numero1, double numero2) {
 		return numero1 / numero2;
 	}
 
-	public int divisao(int numero1, int numero2) {
+	public static int divisao(int numero1, int numero2) {
 		return numero1 / numero2;
 	}
 
-	public double divisao(double numero1, double numero2, double numero3) {
+	public static double divisao(double numero1, double numero2, double numero3) {
 		return numero1 / numero2;
 	}
 
-	public int divisao(int[] array) {
+	public static int divisao(int[] array) {
 
 		int total = 0;
 
