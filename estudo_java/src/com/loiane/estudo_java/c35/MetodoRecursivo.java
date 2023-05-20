608e9ca11b58dad2e8fabd3087fa1eedf75ac42a
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-19 21:13:10 -0400

    added not recursive method

--- estudo_java/src/com/loiane/estudo_java/c35/MetodoRecursivo.java
+++ estudo_java/src/com/loiane/estudo_java/c35/MetodoRecursivo.java
@@ -1,5 +1,16 @@
 package com.loiane.estudo_java.c35;
 
 public class MetodoRecursivo {
-
+	
+	// metodo NÃO recursivo
+	public int calcularFatorial(int number) {
+		int total = 1;
+		for (int i = number; i > 1; i--) {
+			total *= i;
+		}
+		return total;
+	}
+	
+	
+	
 }
