529eb9ae316ef07cdcd1ad7017693f69efbd79fd
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-19 21:17:55 -0400

    changes

--- estudo_java/src/com/loiane/estudo_java/c35/Main.java
+++ estudo_java/src/com/loiane/estudo_java/c35/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		MetodoRecursivo novo = new MetodoRecursivo();
 		
+		System.out.println();
 		
 		
 		
--- estudo_java/src/com/loiane/estudo_java/c35/MetodoRecursivo.java
+++ estudo_java/src/com/loiane/estudo_java/c35/MetodoRecursivo.java
@@ -3,7 +3,7 @@ package com.loiane.estudo_java.c35;
 public class MetodoRecursivo {
 	
 	// metodo NÃO recursivo
-	public int calcularFatorial(int number) {  // cálculo de fatorial através de um loop normal
+	public static int calcularFatorialNormal(int number) {  // cálculo de fatorial através de um loop normal
 		int total = 1;
 		for (int i = number; i > 1; i--) {
 			total *= i;
