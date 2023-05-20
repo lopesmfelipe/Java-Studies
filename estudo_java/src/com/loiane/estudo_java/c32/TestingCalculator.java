c97151dc0eda4af638104de3fadb495e8994d2d7
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-19 20:46:38 -0400

    update

--- estudo_java/src/com/loiane/estudo_java/c32/TestingCalculator.java
+++ estudo_java/src/com/loiane/estudo_java/c32/TestingCalculator.java
@@ -5,11 +5,9 @@ public class TestingCalculator {
 	public static void main(String[] args) {
 
 		Calculator calculator01 = new Calculator();
-		
-		//calculator01.divisao(new int[] {1});
-		
-		System.out.println(calculator01.divisao(new int[] {2,2,2}));
-		
+
+		System.out.println(calculator01.divisao(new int[] { 2, 2, 2 }));
+
 	}
 
 }
