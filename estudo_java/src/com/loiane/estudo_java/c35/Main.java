f5d76b46a810c05e1a6de6dae9588c89f0f36f85
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-20 07:14:56 -0400

    update

--- estudo_java/src/com/loiane/estudo_java/c35/Main.java
+++ estudo_java/src/com/loiane/estudo_java/c35/Main.java
@@ -3,12 +3,11 @@ package com.loiane.estudo_java.c35;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println(MetodoRecursivo.fatorialNaoRecursivo(5));
-		
-		System.out.println(MetodoRecursivo.fatorialRecursivo(5));  // empilha e desempilha
-		
-		
+
+		System.out.println(MetodoRecursivo.fatorialRecursivo(5)); // empilha e desempilha
+
 	}
 
 }
