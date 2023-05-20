19bfb382986ec8fd001bf9bca81646bac97ea437
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-20 04:51:02 -0400

    comments

--- estudo_java/src/com/loiane/estudo_java/c35/MetodoRecursivo.java
+++ estudo_java/src/com/loiane/estudo_java/c35/MetodoRecursivo.java
@@ -15,7 +15,7 @@ public class MetodoRecursivo {
 	public static int fatorialRecursivo(int valorRecebido) {
 		
 		if ( valorRecebido == 0) {
-			return 1;
+			return 1; // aqui ele sai, e já pode fazer as contas, porque todos os métodos dos fatoriais já tem valor definido
 		}
 		return valorRecebido * fatorialRecursivo(valorRecebido - 1);  // entra no método de 'fatorialRecursivo' de valorRecebido -1, quando o valor chegar em zero, vai retornar 1, e voltar a executar o método vezes os métodos com os valores já definidos 
 		// vai se aprofundando, até o fatorial chegar a zero, quando isso acontece, começa a retornar os valores de cada método fatorial, em ordem crescente
