484a6ad99d9d259c1265e091d777399e273c6bbf
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-19 21:20:01 -0400

    comments

--- estudo_java/src/com/loiane/estudo_java/c35/MetodoRecursivo.java
+++ estudo_java/src/com/loiane/estudo_java/c35/MetodoRecursivo.java
@@ -2,8 +2,8 @@ package com.loiane.estudo_java.c35;
 
 public class MetodoRecursivo {
 	
-	// metodo NÃO recursivo
-	public static int calcularFatorialNormal(int number) {  // cálculo de fatorial através de um loop normal
+	// metodo NÃO recursivo || // cálculo de fatorial através de um loop normal
+	public static int calcularFatorialNormal(int number) {  
 		int total = 1;
 		for (int i = number; i > 1; i--) {
 			total *= i;
