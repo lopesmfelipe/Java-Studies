b952db7b74a30a941c63792b4b5712010ccd4f12
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-21 00:49:05 -0400

    new class p36

---
+++ estudo_java/src/com/java_studies/p36/Contact.java
@@ -0,0 +1,9 @@
+package com.java_studies.p36;
+
+public class Contact {
+	
+	
+	
+	
+	
+}
---
+++ estudo_java/src/com/java_studies/p36/Test.java
@@ -0,0 +1,9 @@
+package com.java_studies.p36;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
