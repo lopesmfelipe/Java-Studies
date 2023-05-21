ae6460559080f57742e5aacbe89bad34bc0f817f
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-21 17:52:05 -0400

    added another way to traverse the array.

--- estudo_java/src/com/java_studies/p36/Test.java
+++ estudo_java/src/com/java_studies/p36/Test.java
@@ -16,21 +16,19 @@ public class Test {
 		phone1.setTipo("residential");
 		phone1.setNumero("222222222");
 		phone1.setDdd("+55");
-		
+
 		Phone phone2 = new Phone(); // object
 		phone2.setTipo("personal");
 		phone2.setNumero("33333333");
 		phone2.setDdd("+99");
 
-		
 		Phone[] phones = new Phone[2]; // array
 		phones[0] = phone1; // residential phone object
 		phones[1] = phone2; // personal phone object
-		
-		
+
 		contact.setName("Smaug");
 		contact.setAdress(adr);
-		contact.setPhones(phones);
+		contact.setPhones(phones); // como o atributo de 'contact' para 'phone' é um array, se deve settar um array
 
 		System.out.println(contact.getName());
 
@@ -38,16 +36,21 @@ public class Test {
 			System.out.println(contact.getAdress().getCity());
 		}
 		if (contact != null && contact.getPhones() != null) {
-			
-			for (Phone phone : phones) {
+
+			for (Phone phone : phones) { // para imprimir um array, deve-se percorre-lo com um loop
 				System.out.println(phone.getNumero());
 				System.out.println(phone.getDdd());
 				System.out.println(phone.getTipo());
-
 			}
-			
-			
-			
+
+			System.out.println();
+
+			for (int i = 0; i < phones.length; i++) {
+				System.out.println(phones[i].getNumero());
+				System.out.println(phones[i].getDdd());
+				System.out.println(phones[i].getTipo());
+			}
+
 		}
 
 	}
