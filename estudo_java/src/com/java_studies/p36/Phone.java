aea48a4b60bace72b92918c1bcdc55e65a71c1cc
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-21 07:23:17 -0400

    getters and setters

--- estudo_java/src/com/java_studies/p36/Phone.java
+++ estudo_java/src/com/java_studies/p36/Phone.java
@@ -5,6 +5,24 @@ public class Phone {
 	String tipo;
 	String ddd;
 	String numero;
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getDdd() {
+		return ddd;
+	}
+	public void setDdd(String ddd) {
+		this.ddd = ddd;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
 	
 	
 	
