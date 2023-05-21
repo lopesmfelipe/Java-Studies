45d3d174979e7332eb4b334f6fca88752a977faf
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-21 16:58:20 -0400

    update

--- estudo_java/src/com/java_studies/p36/Test.java
+++ estudo_java/src/com/java_studies/p36/Test.java
@@ -5,32 +5,31 @@ public class Test {
 	public static void main(String[] args) {
 
 		Contact contact = new Contact();
+
 		Adress adr = new Adress();
-		Phone phon = new Phone();
-		
 		adr.setCity("San Francisco");
 		adr.setState("California");
 		adr.setStreetName("Silicon Valley");
 		adr.setZipCode("000000");
 
+		Phone phon = new Phone();
 		phon.setTipo("residential");
 		phon.setNumero("222222222");
 		phon.setDdd("+55");
-		
-		
+
 		contact.setName("Smaug");
 		contact.setAdress(adr);
 		contact.setPhone(phon);
 
 		System.out.println(contact.getName());
-		
+
 		if (contact != null && contact.getAdress() != null) {
 			System.out.println(contact.getAdress().getCity());
 		}
 		if (contact != null && contact.getPhone() != null) {
 			System.out.println(contact.getPhone().getNumero());
 		}
-		
+
 	}
 
 }
