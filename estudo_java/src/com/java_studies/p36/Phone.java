eaf0ddb1f168811dfa9087fde8a9034fe6e7c3d3
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-21 07:23:37 -0400

    formatted

--- estudo_java/src/com/java_studies/p36/Phone.java
+++ estudo_java/src/com/java_studies/p36/Phone.java
@@ -1,30 +1,33 @@
 package com.java_studies.p36;
 
 public class Phone {
-	
+
 	String tipo;
 	String ddd;
 	String numero;
+
 	public String getTipo() {
 		return tipo;
 	}
+
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
+
 	public String getDdd() {
 		return ddd;
 	}
+
 	public void setDdd(String ddd) {
 		this.ddd = ddd;
 	}
+
 	public String getNumero() {
 		return numero;
 	}
+
 	public void setNumero(String numero) {
 		this.numero = numero;
 	}
-	
-	
-	
-	
+
 }
