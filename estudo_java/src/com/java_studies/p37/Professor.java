799c5a5c1a1d126d97871f8549b3b509685dad9a
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-21 21:48:35 -0400

    addes new attributes in Professor class.

--- estudo_java/src/com/java_studies/p37/Professor.java
+++ estudo_java/src/com/java_studies/p37/Professor.java
@@ -2,7 +2,14 @@ package com.java_studies.p37;
 
 public class Professor {
 	
+	private double wage;
+	private String name;
+	private String phone;
+	private String departamento;
 	
+	private static double calcularSalarioLiquido() {
+		return 0;
+	}
 	
 	
 }
