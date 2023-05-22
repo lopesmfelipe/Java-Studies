2f8149f28747cd57a23bdf526fb6363a1c28bb09
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-21 21:51:19 -0400

    new methods

--- estudo_java/src/com/java_studies/p37/Professor.java
+++ estudo_java/src/com/java_studies/p37/Professor.java
@@ -7,9 +7,41 @@ public class Professor {
 	private String phone;
 	private String departamento;
 	
+
+	public double getWage() {
+		return wage;
+	}
+
+	public void setWage(double wage) {
+		this.wage = wage;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+	
 	private static double calcularSalarioLiquido() {
 		return 0;
 	}
-	
 	
 }
--- estudo_java/src/com/java_studies/p37/Student.java
+++ estudo_java/src/com/java_studies/p37/Student.java
@@ -9,6 +9,38 @@ public class Student {
 	
 	
 	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String[] getCourses() {
+		return courses;
+	}
+
+	public void setCourses(String[] courses) {
+		this.courses = courses;
+	}
+
+	public double[][] getNotas() {
+		return notas;
+	}
+
+	public void setNotas(double[][] notas) {
+		this.notas = notas;
+	}
+
 	public static double calcularMedia() {
 		return 0;
 	}
