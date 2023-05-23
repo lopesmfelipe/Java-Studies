7b6466d3b1aa5e55889615cc54f1eb7a51d93eb5
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-23 03:25:30 -0400

    some deleted classes

--- estudo_java/src/com/java_studies/p40/Student.java
+++ estudo_java/src/com/java_studies/p40/Student.java
@@ -6,12 +6,11 @@ public class Student extends Pessoa {
 	private double[][] notas;
 
 	public Student(String name, String phone) {
-		super(name, phone); // vai chamar o construtor da superclasse com super e definir os parâmetros com
-							// os recebidos aqui
+		super(name, phone); 
 	}
 
 	public Student() {
-		super(); // vai chamar o construtor sem parâmetros da superclasse
+		super(); 
 	}
 
 	public String[] getCourses() {
@@ -30,17 +29,9 @@ public class Student extends Pessoa {
 		this.notas = notas;
 	}
 
-	public static double calcularMedia() {
-		return 0;
-	}
-
-	public static String verificarAprovacao() {
-		return "";
-	}
-
 	public void algumaCoisa() {
-		this.setName("Trevor");  // por herança pode utilizar os métods da superClass que extends
-		super.setName("Trevor"); // só vai mostrar os métodos da Superclass
+		this.setName("Trevor");  
+		super.setName("Trevor"); 
 		
 	}
 }
