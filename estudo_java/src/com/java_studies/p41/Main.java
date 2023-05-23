aa75bf51f01f462ac05973ed1227d3d961f5b2ad
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-23 03:42:54 -0400

    update p41

--- estudo_java/src/com/java_studies/p41/Main.java
+++ estudo_java/src/com/java_studies/p41/Main.java
@@ -1,12 +1,21 @@
 package com.java_studies.p41;
 
+import com.java_studies.p40.Pessoa;
+import com.java_studies.p40.Professor;
+import com.java_studies.p40.Student;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+
+		Pessoa pessoa01 = new Pessoa();
+		Pessoa student01 = new Student();
+		Pessoa professor01 = new Professor();
+
+		pessoa01.setPhone("0000000");
+		student01.setPhone("1111111111");
+		professor01.setPhone("999999999");
+
 	}
 
-	
-	
-	
 }
