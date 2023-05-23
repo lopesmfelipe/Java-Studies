019e76fbe960272830823c43b2c279bbe7fd600c
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-23 02:38:29 -0400

    update

--- estudo_java/src/com/java_studies/p40/Main.java
+++ estudo_java/src/com/java_studies/p40/Main.java
@@ -6,13 +6,17 @@ public class Main {
 		
 		Pessoa pessoa01 = new Pessoa();
 		Pessoa student01 = new Student();
-		Pessoa Professor01 = new Professor();
+		Pessoa professor01 = new Professor();
 		
 
-		
-		
-		
-		
+		pessoa01.setAdress("New York");
+		student01.setAdress("Los Angeles");
+		professor01.setAdress("Las Vegas");
+
+		System.out.println(pessoa01.obterEtiquetaEndereco());
+		System.out.println(student01.obterEtiquetaEndereco());
+		System.out.println(professor01.obterEtiquetaEndereco());
+
 		
 	}
 
