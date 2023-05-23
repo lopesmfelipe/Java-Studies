41c3458d264069559f005c855814ae13f07c4331
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-22 21:52:01 -0400

    new classes

---
+++ estudo_java/src/com/java_studies/p39/Pessoa.java
@@ -0,0 +1,52 @@
+package com.java_studies.p39;
+
+public class Pessoa {
+
+	private String name; // classes que extends essa aqui, teram acesso aos seus atributos através dos
+							// seus métodos get/set, já que os atributos estão privados
+	private String adress;
+	private String cpf;
+	private String phone;
+
+	public Pessoa() {
+	}
+
+	public Pessoa(String name, String phone) { // construtor com parâmetros
+		super();
+		this.name = name;
+		this.phone = phone;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+}
