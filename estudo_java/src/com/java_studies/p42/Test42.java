91a497b1d1f81836a703910baa0fe747b3c09ea1
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-23 22:20:26 -0400

    update

--- estudo_java/src/com/java_studies/p42/Test42.java
+++ estudo_java/src/com/java_studies/p42/Test42.java
@@ -4,7 +4,7 @@ public class Test42 {
 
 	public static void main(String[] args) {
 		
-		Pessoa nPessoa = new Pessoa();
+		Pessoa nPessoa = new Pessoa();  // declara o tipo pessoa mas instancia os tipos diferentes
 		Pessoa nStudent = new Student();
 		Pessoa nProfessor = new Professor();
 		
