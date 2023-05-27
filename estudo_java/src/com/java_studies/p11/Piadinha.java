fe37ac171974cf58930e8e706de5ec0d47e5e946
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-26 23:59:55 -0400

    formatted code

--- estudo_java/src/com/java_studies/p11/Piadinha.java
+++ estudo_java/src/com/java_studies/p11/Piadinha.java
@@ -3,15 +3,13 @@ package com.java_studies.p11;
 public class Piadinha {
 
 	public static void main(String[] args) {
-		
-		int oct31 = 031;  // número em octal
-		
-		int dez25 = 25;   // número em decimal
-		
+
+		int oct31 = 031; // número em octal
+
+		int dez25 = 25; // número em decimal
+
 		System.out.println(oct31 == dez25);
-		
-		
-		
+
 	}
 
 }
