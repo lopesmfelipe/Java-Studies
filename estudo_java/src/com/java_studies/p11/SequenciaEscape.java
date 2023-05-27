caeae777861b88c4c0e74bf031c273f919af9823
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-27 00:00:12 -0400

    formatted code

--- estudo_java/src/com/java_studies/p11/SequenciaEscape.java
+++ estudo_java/src/com/java_studies/p11/SequenciaEscape.java
@@ -3,15 +3,13 @@ package com.java_studies.p11;
 public class SequenciaEscape {
 
 	public static void main(String[] args) {
-		
+
 		// "The world is yours!!!"
-		System.out.println("\"The world is yours!!!\"");  // Barra invertida para formatar e fazer as aspas aparecerem
-		
+		System.out.println("\"The world is yours!!!\""); // Barra invertida para formatar e fazer as aspas aparecerem
+
 		// 1\4
-		System.out.println("1\\4");  // Barra invertida para printar "1\4" corretamente
-		
-		
-		
+		System.out.println("1\\4"); // Barra invertida para printar "1\4" corretamente
+
 	}
 
 }
