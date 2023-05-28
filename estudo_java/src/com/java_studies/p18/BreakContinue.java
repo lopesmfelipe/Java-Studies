4cb9ed0c75ddc66cc23b73e990f236c55f1248cc
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-27 23:55:33 -0400

    formatted code

--- estudo_java/src/com/java_studies/p18/BreakContinue.java
+++ estudo_java/src/com/java_studies/p18/BreakContinue.java
@@ -5,44 +5,44 @@ import java.util.Scanner;
 public class BreakContinue {
 
 	public static void main(String[] args) {
-		
+
 		// Loop com break:
 		Scanner scanner1 = new Scanner(System.in);
-		
+
 		System.out.println("Insira um número:");
 		int numero1 = scanner1.nextInt();
-		
+
 		System.out.println("Insira um número limite:");
 		int limite1 = scanner1.nextInt();
-		
+
 		for (int i = numero1; i <= limite1; i++) {
-			
-			if (i % 7 == 0){
+
+			if (i % 7 == 0) {
 				break;
 			} else {
 				System.out.println(i);
 			}
-		} 
-		
-		//Loop com continue:
+		}
+
+		// Loop com continue:
 		Scanner scanner2 = new Scanner(System.in);
-		
+
 		System.out.println("Insira um número:");
 		int numero2 = scanner2.nextInt();
-		
+
 		System.out.println("Insira um número limite:");
 		int limite2 = scanner2.nextInt();
-		
+
 		for (int i = numero2; i <= limite2; i += 1) {
-			
-			if (i % 7 == 0){
+
+			if (i % 7 == 0) {
 				continue;
-			} 
-		    System.out.println("O valor de i é: " + i);
+			}
+			System.out.println("O valor de i é: " + i);
 		}
-		
-		// GO TO: 
-		for (int i = 0; i <= 5; i ++) {
+
+		// GO TO:
+		for (int i = 0; i <= 5; i++) {
 			rotulo1: {
 				rotulo2: {
 					rotulo3: {
@@ -61,8 +61,8 @@ public class BreakContinue {
 				}
 				System.out.println("rotulo 1");
 			}
-		 System.out.println(i);
+			System.out.println(i);
 		}
-		
+
 	}
 }
