20d07c0a8721b9ce9711a0483b51b8fc3b570623
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-27 23:59:00 -0400

    Formatted

--- estudo_java/src/com/java_studies/p28/Main.java
+++ estudo_java/src/com/java_studies/p28/Main.java
@@ -1,20 +1,11 @@
 package com.java_studies.p28;
-									 import com.java_studies.p11.*;
+
+import com.java_studies.p11.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
 	}
 
 }
