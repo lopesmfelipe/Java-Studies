84cc0031e93c03dda53fac1acd53a17c7ba500d4
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-27 23:58:22 -0400

    Formatted

--- estudo_java/src/com/java_studies/p25/Carro.java
+++ estudo_java/src/com/java_studies/p25/Carro.java
@@ -1,19 +1,16 @@
 package com.java_studies.p25;
 
 public class Carro {
-	
-	String marca; 
+
+	String marca;
 	String modelo;
 	int numeroPassageiros;
 	double capacidadeCombustivel;
 	double consumoCombustivel;
 	double velocidadeMaxima;
-	
+
 	void exibirAutonomiaCombustivel() {
 		System.out.println("A autonoma do carro é: " + capacidadeCombustivel * consumoCombustivel + "km");
 	}
-	
-	
-	
-	
+
 }
