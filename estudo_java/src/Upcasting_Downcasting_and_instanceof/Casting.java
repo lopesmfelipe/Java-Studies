c7734e9feaa073a0a0201a9f484aa1ed53aafa93
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-27 23:54:15 -0400

    update

--- estudo_java/src/Upcasting_Downcasting_and_instanceof/Casting.java
+++ estudo_java/src/Upcasting_Downcasting_and_instanceof/Casting.java
@@ -3,9 +3,20 @@ package Upcasting_Downcasting_and_instanceof;
 public class Casting {
 
 	public static void main(String[] args) {
+
+		Octopus theOctopus = new Octopus(); // UPCASTING
+
+		doAnimalStuff(theOctopus);
 		
-		
-		
+		Dragon theDragon = new Dragon();
 	}
 
+	public static void doAnimalStuff(Animal animal) {  // neste método ocorre 'CASTING' já que ele recebe um objeto de qualquer subclasse que extend Animal. 
+		animal.animalMove();
+	}
+	
+	
+	
+	
+	
 }
