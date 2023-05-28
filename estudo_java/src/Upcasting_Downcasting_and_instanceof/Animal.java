387dcc58d04935627a4e30a1037c548aee206017
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-27 23:54:07 -0400

    update

--- estudo_java/src/Upcasting_Downcasting_and_instanceof/Animal.java
+++ estudo_java/src/Upcasting_Downcasting_and_instanceof/Animal.java
@@ -1,11 +1,11 @@
 package Upcasting_Downcasting_and_instanceof;
 
 public class Animal {
-	
+
 	String name;
-	
-	public void move() {
+
+	public void animalMove() {
 		System.out.println("I'm an Animal");
 	}
-	
+
 }
