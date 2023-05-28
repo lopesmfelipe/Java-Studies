2cce25837337ac8511322df12876a3e00824211c
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-27 23:59:33 -0400

    Formatted

--- estudo_java/src/com/java_studies/p31alt/TesteCarroAlt.java
+++ estudo_java/src/com/java_studies/p31alt/TesteCarroAlt.java
@@ -5,15 +5,11 @@ import com.java_studies.p30.Carro;
 public class TesteCarroAlt {
 
 	public static void main(String[] args) {
-		
+
 		Carro honda = new Carro();
-		
 
 		System.out.println();
-		
+
 	}
-	
-	
-	
-	
+
 }
