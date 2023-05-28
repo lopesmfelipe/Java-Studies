f1909c9be05b210fca264f21cabd8428c7903531
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-27 23:54:35 -0400

    update

--- estudo_java/src/Upcasting_Downcasting_and_instanceof/Octopus.java
+++ estudo_java/src/Upcasting_Downcasting_and_instanceof/Octopus.java
@@ -1,5 +1,14 @@
 package Upcasting_Downcasting_and_instanceof;
 
-public class Octopus {
+public class Octopus extends Animal {
+
+	@Override
+	public void animalMove() {
+		System.out.println("Swimming");
+	}
+
+	public void attack() {
+		System.out.println("Ink");
+	}
 
 }
