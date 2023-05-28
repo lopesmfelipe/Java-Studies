5104d8c26f65bd42febdd71b2561ee5b106dafad
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-27 23:54:24 -0400

    update

--- estudo_java/src/Upcasting_Downcasting_and_instanceof/Dragon.java
+++ estudo_java/src/Upcasting_Downcasting_and_instanceof/Dragon.java
@@ -1,5 +1,14 @@
 package Upcasting_Downcasting_and_instanceof;
 
-public class Dragon {
+public class Dragon extends Animal {
+
+	@Override
+	public void animalMove() {
+		System.out.println("Flying");
+	}
+
+	public void attack() {
+		System.out.println("Fire");
+	}
 
 }
