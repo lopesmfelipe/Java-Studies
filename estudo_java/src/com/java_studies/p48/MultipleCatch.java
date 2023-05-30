fd673509eb8dda305b4912fe875bdbbff24163ad
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-30 04:05:37 -0400

    Added try and catch

--- estudo_java/src/com/java_studies/p48/MultipleCatch.java
+++ estudo_java/src/com/java_studies/p48/MultipleCatch.java
@@ -4,7 +4,17 @@ public class MultipleCatch {
 
 	public static void main(String[] args) {
 		
+		int[] numbers = {90, 100, 5, 3, 1};
+		int[] denominador = {2, 0, 2, 2, 9};
 		
+		for(int i = 0; i < numbers.length; i++) {
+			try {
+			System.out.println(numbers[i] + "/" + denominador[i] + " = " + (numbers[i]/denominador[i]));
+			}
+			catch(ArithmeticException exception1) {
+				System.out.println("Error - division by zero");
+			}
+		}
 		
 		
 	}
