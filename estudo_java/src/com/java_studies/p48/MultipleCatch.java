f247a30530dc6af7d0d8bde2882daa290f2696a8
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-30 05:00:35 -0400

    Added generic catch and catch with two exception types.

--- estudo_java/src/com/java_studies/p48/MultipleCatch.java
+++ estudo_java/src/com/java_studies/p48/MultipleCatch.java
@@ -3,20 +3,21 @@ package com.java_studies.p48;
 public class MultipleCatch {
 
 	public static void main(String[] args) {
-		
-		int[] numbers = {90, 100, 5, 3, 1};
-		int[] denominador = {2, 0, 2, 2, 9};
-		
-		for(int i = 0; i < numbers.length; i++) {
+
+		int[] numbers = { 90, 100, 5, 3, 1 };
+		int[] denominador = { 2, 0, 2, 2 };
+
+		for (int i = 0; i < numbers.length; i++) {
 			try {
-			System.out.println(numbers[i] + "/" + denominador[i] + " = " + (numbers[i]/denominador[i]));
-			}
-			catch(ArithmeticException exception1) {
-				System.out.println("Error - division by zero");
+				System.out.println(numbers[i] + "/" + denominador[i] + " = " + (numbers[i] / denominador[i]));
+			} catch (ArrayIndexOutOfBoundsException | ArithmeticException e) { // two similar exceptions can be on the
+																				// same 'catch' with "or" "|"
+				System.out.println("An erro ocurred.");
+			} catch (Throwable y) { // Generic catch goes last
+				System.out.println("An error occurred, please wait a minute and try again");
 			}
 		}
-		
-		
+
 	}
 
 }
