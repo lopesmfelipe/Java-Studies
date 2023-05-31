9055dbe22275f7c9f4bc32b91997e4b4c2ac5308
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-05-30 21:12:33 -0400

    Added new package.

--- estudo_java/src/com/java_studies/p49/PegadinhaFinallyExit.java
+++ estudo_java/src/com/java_studies/p49/PegadinhaFinallyExit.java
@@ -1,9 +1,9 @@
 package com.java_studies.p49;
 
-public class TestingFinally {
+public class PegadinhaFinallyExit {
 
 	public static void main(String[] args) {
-		
+
 		int[] numbers = { 90, 100, 5, 3, 1 };
 		int[] denominador = { 2, 0, 2, 2 };
 
@@ -11,16 +11,17 @@ public class TestingFinally {
 			try {
 				System.out.println(numbers[i] + "/" + denominador[i] + " = " + (numbers[i] / denominador[i]));
 			} catch (ArrayIndexOutOfBoundsException | ArithmeticException e) { // two similar exceptions can be on the
-																				// same 'catch' with "or" "|"
-				System.out.println("An erro ocurred.");
+				System.out.println("An error ocurred.");
+				System.exit(0);
 			} catch (Throwable y) { // Generic catch goes last
 				System.out.println("An error occurred, please wait a minute and try again");
-			}
-			finally {
+				System.exit(0);
+			} finally {
 				System.out.println("This line aways is printed");
+				System.out.println();
 			}
+
 		}
-		
 	}
 
 }
---
+++ estudo_java/src/com/java_studies/p50/Main.java
@@ -0,0 +1,11 @@
+package com.java_studies.p50;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
