48e87b16d019dae76d097247b6bd11d51a508d79
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-06-01 01:46:28 -0400

    Added comments.

--- estudo_java/src/com/java_studies/p50/Main.java
+++ estudo_java/src/com/java_studies/p50/Main.java
@@ -4,7 +4,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		
+		int[] numbers = { 90, 100, 5, 3, 1 };
+		int[] denominador = { 2, 0, 2, 2, 6 };
+
+		for (int i = 0; i < numbers.length; i++) {
+			try {
+				System.out.println(numbers[i] + "/" + denominador[i] + " = " + (numbers[i] / denominador[i]));
+			} catch (Exception e) {
+				e.getMessage();  // Throwable class method
+				e.printStackTrace(); // Throwable class method
+			} 
+		}
 		
 	}
 
