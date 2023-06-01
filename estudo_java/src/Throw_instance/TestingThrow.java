7fb3700a3dc08a168ba26cbb70028141da0ceb34
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-06-01 00:29:49 -0400

    It was throwed a new exception.

--- estudo_java/src/Throw_instance/TestingThrow.java
+++ estudo_java/src/Throw_instance/TestingThrow.java
@@ -4,8 +4,16 @@ public class TestingThrow {
 
 	public static void main(String[] args) {
 		
+		/* você pode gerar uma exceção de qualquer tipo que seja(e definir uma mensagem especifica), para ser lançado 
+		 * quando um comportamento não permitido ocorrer */
 		
+		/*Example*/
+		double number1 = 29;
+		double number2 = 999;
 		
+		if (number1 > 700 | number2 > 700) { /*caso a condição não seja cumprida, é lançada uma exceção*/
+		throw new IllegalArgumentException("Foi lançada uma exceção"); 
+		}
 		
 	}
 
